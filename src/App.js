4c5ab91d395e80981a825e0f21cac93fe120ee7d
Stultz-e <Chris.stultz@pinnacledesignlab.com>
2021-04-13 17:55:04 -0600

    update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1> Hats PAGE </h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HomePage></HomePage>
+          <Route path='/' component={HomePage} />
       </div>
     );
   }
