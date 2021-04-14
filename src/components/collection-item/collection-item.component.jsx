623babccdbd4f6fad4d0d48d2b922227fbc02345
Stultz-e <Chris.stultz@pinnacledesignlab.com>
2021-04-14 14:13:37 -0600

    card

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -11,6 +11,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         }}
         
         >
+        <div className='collection-footer'>
+            <span className='name'>{ name }</span>
+            <span className='price'>{ price }</span>
+        </div>
+
         </div>
     </div>
 )
+
+export default CollectionItem;
