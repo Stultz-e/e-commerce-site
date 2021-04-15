81c80c6a2e1ad1c226a1ae9dda2f4c3b1286d680
Stultz-e <Chris.stultz@pinnacledesignlab.com>
2021-04-14 18:14:32 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { Switch , Route } from 'react-router-dom';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div>
-        <switch>
+      <Header />
+        <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-        </switch>
+        </Switch>
       </div>
     );
   }
-}
 
 export default App;
